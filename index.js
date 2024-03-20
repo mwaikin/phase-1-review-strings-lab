ae5e9870e35596dfbabca6162e2a981d63410e4a
mwaikin <mwaikingori10@gmail.com>
2024-03-20 11:22:25 +0300

    complete string labs

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code in this file!
+// Wriconst currentUser = 'Grace Hopper';
+const currentUser = 'Welcome to Flatbook,';
+const welcomeMessage = 'Welcome to Flatbook, currentUser!';
+const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, currentUser!';
+const shortGreeting = 'Welcome, currentUser!';
+
+
+
